f5fa4d9d27acb873127ef1b1b966dd10e7802f39
Clinton Farleigh <farleigh@gmail.com>
2020-09-13 07:14:58 -0700

    Add parameter checks

--- calc_gdp_rate.sh
+++ calc_gdp_rate.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-if [ -z $1 ]
-then
-  echo "Missing file name"
+if [ -z $1 ]; then
+  echo "Usage: ./calc_gdp_rate.sh file"
   exit
 fi
 
-awk -F',' 'BEGIN{ print "Year,Percent Increase"} {printf("%s,",$1); system("./calc_percent_increase.sh " $2 " " $3)}' $1  > output.csv
+echo "Writing output to >>> $2"
+awk -F',' 'BEGIN{ print "Year,Percent Increase"} {printf("%s,",$1); system("./calc_percent_increase.sh " $2 " " $3)}' $1  > $2
--- calc_percent_increase.sh
+++ calc_percent_increase.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [ -z $1 ]; then
+  echo "Usage: ./calc_percent_increase.sh new_value old_value"
+  exit
+fi
+
 # $1 = year, $2 = current value, $3 = previous value
 function calcPercentIncrease {
   local percentIncrease=`awk "BEGIN {printf \"%.2f\", (${1}-${2})/${2}*100}"`
